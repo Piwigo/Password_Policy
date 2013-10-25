73033d3c7203885730c1b1b6f7a9499436f411c8
Eric <Eric@piwigo.org>
2013-10-25 13:07:45 +0000

    Next version is 2.5.2 :
    Update ru_RU, thanks to : Konve
    Update de_DE, thanks to : fs101299
    Update tr_TR, thanks to : LazBoy
    Update da_DK, thanks to : Kaare
    Update pt_BR, thanks to : flaviove
    Update lv_LV, thanks to : agrisans
    Update es_ES, thanks to : jpr928
    
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@25134 68402e56-0260-453c-a942-63ccdbb3a9ee

--- changelog.txt.php
+++ changelog.txt.php
@@ -8,5 +8,14 @@ Plugin Name: Password Policy
 -- 2.5.1 : Improve workflow when account is locked and a password renewal is set
            Missing translation keys
            Update it_IT, thanks to : Ericnet
+           
+-- 2.5.2 : Update ru_RU, thanks to : Konve
+           Update de_DE, thanks to : fs101299
+           Update tr_TR, thanks to : LazBoy
+           Update da_DK, thanks to : Kaare
+           Update pt_BR, thanks to : flaviove
+           Update lv_LV, thanks to : agrisans
+           Update es_ES, thanks to : jpr928
+           
 */
 ?>
