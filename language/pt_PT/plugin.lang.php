6058706e0b5417dfc531f4965cf862ec6d3ba31a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-26 08:34:16 +0000

    [password_policy] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@25163 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_PT/plugin.lang.php
+++ language/pt_PT/plugin.lang.php
@@ -53,4 +53,37 @@ $lang['PP_PwdReset_Done'] = 'Senha renovada';
 $lang['PP_PwdReset_NA'] = 'Senha original';
 $lang['PP %d user pwdreseted'] = 'Renovação de senha requerida para %d um utilizador';
 $lang['PP %d users pwdreseted'] = 'Renovação de senha requerida para %d utilizadores';
+$lang['PP_Webmaster is not unlockable'] = 'Os utilizadores webmaster não podem ser bloqueados ou desbloqueados';
+$lang['PP_You cannot unlock your account'] = 'Não pode desbloquear a sua conta';
+$lang['PP_You need to confirm unlock'] = 'Deverá confirmar o desbloqueio (sinalize a caixa)';
+$lang['PP_passwtestTitle'] = 'Teste da complexidade da senha';
+$lang['PP_passwtestTitle_d'] = 'Escreva a senha a testar e depois clique em &quot;Score calculation&quot; para ver o resultado.';
+$lang['PP %d user unlocked'] = '%d utilizador desbloqueado';
+$lang['PP %d users unlocked'] = '%d utilizadores desbloqueados';
+$lang['PP_Admins is not unlockable'] = 'Admin não pode ser bloqueada ou desbloqueada';
+$lang['PP_Default user is not unlockable'] = 'Utilizador por defeito não pode ser bloqueado ou desbloqueado';
+$lang['PP_Generic is not unlockable'] = 'Utilizadores genéricos não podem ser bloqueados ou desbloqueados';
+$lang['PP_Guest is not unlockable'] = 'Visitantes não podem ser bloqueados ou desbloqueados';
+$lang['PP_LockedUsers'] = 'Estado de bloqueio';
+$lang['PP_LoginAttempts'] = 'Gestão de tentativas de login';
+$lang['PP_LoginAttempts_d'] = 'Esta opção ativa a gravação de tentativas de tentativas falhadas de entrada na galeria devido a senha errada e bloqueia automaticamente a conta do utilizador em causa. Foi concebido para contabilizar tentativas de possíveis ataques de hackers utilizando dicionários de senhas. <br/><br/>
+<b style="color: red;&quot;>Webmaster, contas genéricas e convidados estão excluídos desta função. </b><br/>
+Ao ativar, você vai ser capaz de especificar o número máximo de tentativas permitidas antes do bloqueio e defina uma mensagem personalizada para o utilizador o que bloqueou sua conta. Esta mensagem é exibida apenas se o utilizador continuar a tentar a identificação após bloqueio. <br/>
+Para desbloquear as contas, vai ao interface de gerenciamento de utilizador para a galeria. Encontrará uma nova coluna indicando o status de contas com um símbolo vermelho (para conta bloqueada) e verde (para a conta desbloqueada). Selecione uma ou mais contas para desbloquear e clique no botão Desbloquear existente para esta finalidade.';
+$lang['PP_Max number of failed attempts'] = 'Número máximo de tentativas';
+$lang['PP_Max number of failed attempts_d'] = 'O número 0 desativa a contegem de tentativas';
+$lang['PP_Password_Enforced'] = 'Reforçar o nível de segurança das senhas ';
+$lang['PP_PwdResetTitle_d'] = 'Ativando esta opção irá adicionar uma nova funcionalidade no painel de gerenciamento de utilizadores do Piwigo (Gerenciar utilizadores), que pode ser aplicada na renovação da senha dos utilizadores selecionados. E adição, uma nova coluna é adicionada para mostrar o estado das senhas, para cada um deles, com os seguintes valores:<br/>
+- Senha a ser renovada: Foi solicitado um pedido de renovação de senha<br/>.
+- Senha renovada: A senha foi renovada depois de solicitado <br/>.
+- Senha Original:. A senha original escolhida no momento da criação da conta e nunca foi solicitada renovação<br/>
+<b style=&quot;color: red;&quot;> Contas do Webmaster, genéricas e convidados estão excluídas desta função.</b><br/>
+Esses utilizadores serão redirecionados automaticamente para sua página personalizada quando das suas ligações até que alterem a sua senha e uma mensagem explícita será exibida nesta página.';
+$lang['PP_Unlock'] = 'Desbloquear';
+$lang['PP_Unlock selected users'] = 'Desbloquear os utilizadores selecionados';
+$lang['PP_User Locked'] = 'Utilizador bloqueado';
+$lang['PP_User Not Locked'] = 'Utilizador não bloqueado';
+$lang['PP_UserLocked_Custom_Txt'] = 'Personalização da mensagem informativa para o utilizador bloqueado';
+$lang['PP_UserLocked_Custom_Txt_d'] = 'Aqui você pode alterar o texto da mensagem exibida ao utilizador cuja conta foi bloqueada. Para usar vários idiomas, pode usar a  descrição extensa das etiquetas das extensões se ativa.';
+$lang['PP_User_Account_Locked_Txt'] = 'Desculpe, por motivo de segurança o seu acesso à galeria foi bloqueado devido a multiplas tentativas de entrada. Isto pode ser devido a uma tentativa de invasão da sua conta. Por favor, contate o administrador do site para solicitar o desbloqueio.';
 ?>
