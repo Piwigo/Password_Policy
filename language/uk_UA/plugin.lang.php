cef375f3a34254ed6d2a5f7a36d72a39ffdbfab4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-10 08:43:39 +0000

    [password_policy] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@25910 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PP_Unlock'] = 'Розблоковано';
+$lang['PP_PasswordTest'] = 'Розрахунок балів:';
+$lang['PP_PwdReset'] = 'Оновлення пароля';
+$lang['PP_LockedUsers'] = 'Стан блокування';
+$lang['PP_Default user is not unlockable'] = 'За замовчуванням користувач не може бути заблокований і не може бути розблокований';
+$lang['PP_User Locked'] = 'Користувач заблокований';
+$lang['PP_User Not Locked'] = 'Користувач не заблокований';
+$lang['PP_UserLocked_Custom_Txt'] = 'Налаштовування інформаційного повідомлення до заблокованих користувачів';
+$lang['PP_Webmaster cannot be pwdreset'] = 'Відновлення пароля не можна настроїти для облікового запису веб-майстрів!';
+$lang['PP_Webmaster is not unlockable'] = 'Аккаунт веб-майстрів не може бути заблокований і не може бути розблокований';
+$lang['PP_You cannot pwdreset your account'] = 'Відновлення пароля не можна встановити через свій обліковий запис!';
+$lang['PP_You cannot unlock your account'] = 'Ви не можете розблокувати свій власний обліковий запис';
+$lang['PP_You need to confirm pwdreset'] = 'Необхідно підтвердити пароль відновлення (прапорець)!';
+$lang['PP_You need to confirm unlock'] = 'Необхідно підтвердити розблокування (прапорець)!';
+$lang['PP_passwtestTitle_d'] = 'Введіть пароль, щоб перевірити і клацніть на &quot;Розрахунок балів&quot; щоб побачити результат.';
+$lang['PP_passwtestTitle'] = 'Тестування складності пароля';
+$lang['PP_save_config'] = 'Налаштування збережені.';
+$lang['PP_submit'] = 'Представити настройки';
+$lang['PP_User_Account_Locked_Txt'] = 'Вибачте, з міркувань безпеки ваш доступ до галереї був заблокований через велику кількість невдалих спроб входу. Це може бути пов’язано з зламом вашого рахунку. Будь ласка, зверніться до адміністратора сайту з проханням розблокувати.';
+$lang['PP_Unlock selected users'] = 'Розблокувати обраних користувачів';
+$lang['PP_Disable'] = 'Вимкнути (за замовчуванням)';
+$lang['PP_Password_Reset_Msg'] = 'Будь ласка, поміняйте свій пароль!';
+$lang['PP_PwdReset_Done'] = 'Пароль оновлюється';
+$lang['PP_PwdReset_NA'] = 'Оригінальний пароль';
+$lang['PP_ScoreTest'] = 'Результат:';
+$lang['PP_Enable'] = 'Увімкнути';
+$lang['PP_Error_Password_Mandatory'] = 'Безпека: Пароль є обов’язковим!';
+$lang['PP_LoginAttempts'] = 'Управління невдалих спроб входу';
+$lang['PP_Password reset selected users'] = 'Запитувати пароль відновлення для вибраних користувачів';
+$lang['PP_Password_Enforced'] = 'Посилення рівня безпеки паролів';
+$lang['PP_Password_Enforced_true'] = 'Увімкнути. Мінімальна оцінка:';
+$lang['PP %d user pwdreseted'] = 'Пароль відновлення, необхідний для користувача %d';
+$lang['PP %d user unlocked'] = '%d користувач розблокований';
+$lang['PP %d users pwdreseted'] = 'Пароль відновлення необхідний користувачам %d';
+$lang['PP %d users unlocked'] = '%d користувачі розблоковані';
+$lang['PP_AdminPassword_Enforced'] = 'Звернення до адміністраторів';
+$lang['PP_Admins cannot be pwdreset'] = 'Відновлення пароля не можна встановити для аккаунта адміністраторів!';
+?>
