f4c2bf85254b39f99128cdc88e3dcc3610ff7b1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-01 06:04:25 +0000

    [password_policy] Update nl_NL, thanks to : eijdenberg
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@27116 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,35 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PP_Disable'] = 'Uitschakelen (standaard)';
+$lang['PP_Enable'] = 'Inschakelen';
+$lang['PP_Error_Password_Mandatory'] = 'Beveiliging: Wachtwoord is verplicht!';
+$lang['PP_Admins is not unlockable'] = 'Beheerders kunnen niet vergrendeld en ontgrendeld worden ';
+$lang['PP_Default user is not unlockable'] = 'Standaard gebruiker kan niet vergrendeld en ontgrendeld worden';
+$lang['PP_Default user cannot be pwdreset'] = 'Wachtwoord vernieuwing kan niet worden ingesteld voor het standaard gebruikers account!';
+$lang['PP_Admins cannot be pwdreset'] = 'Wachtwoord vernieuwing kan niet worden ingesteld voor beheerders account!';
+$lang['PP_AdminPassword_Enforced'] = 'Toepassen bij beheerders';
+$lang['PP %d users unlocked'] = '%d gebruikers ontgrendeld';
+$lang['PP %d users pwdreseted'] = 'Wachtwoord vernieuwing nodig voor %d gebruikers';
+$lang['PP %d user unlocked'] = '%d gebruiker ontgrendeld';
+$lang['PP %d user pwdreseted'] = 'Wachtwoord vernieuwing nodig voor %d gebruiker';
+?>
