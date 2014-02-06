b39d51db1fba9539dfaa82c322e6cf031448af4e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-06 22:00:11 +0000

    [password_policy] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@27250 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -27,7 +27,7 @@ $lang['PP_save_config'] = 'Opsætning er gemt.';
 $lang['PP_Disable'] = ' Deaktiver (standard)';
 $lang['PP_Enable'] = ' Aktiver';
 $lang['PP_Support_txt'] = 'Officiel support af denne plugin er kun tilgængelig i disse Piwigo-forumemner:<br>
-<a href="http://piwigo.org/forum/viewtopic.php?id=" onclick="window.open(this.href);return false;">Engelsk forum - http://piwigo.org/forum/viewtopic.php?id=</a>';
+<a href="http://piwigo.org/forum/viewtopic.php?id=22863" onclick="window.open(this.href);return false;">Engelsk forum - http://piwigo.org/forum/viewtopic.php?id=22863</a>';
 $lang['PP_PasswordTest'] = 'Adgangskodetest:';
 $lang['PP_ScoreTest'] = 'Resultat:';
 $lang['PP_Error_Password_Mandatory'] = 'Sikkerhed: En adgangskode er påkrævet!';
@@ -56,27 +56,28 @@ $lang['PP %d user pwdreseted'] = 'Fornyelse af adgangskode krævet for brugeren 
 $lang['PP %d users pwdreseted'] = 'Fornyelse af adgangskode krævet for brugerne %d';
 $lang['PP_passwtestTitle'] = 'Tester adgangskodens kompleksitet';
 $lang['PP_passwtestTitle_d'] = 'Skriv adgangskoden, der skal testes, og klik dernæst på &quot;Beregn score&quot; for at se resultatet.';
-$lang['PP_passwTitle_d'] = 'Aktivering af denne valgmulighed gør det til at krav at have en adgangskode ved registreringen, og kræver at den valgte adgangskode opfylder et minimalt kompleksitetsniveau.  Hvis tærsklen ikke opfyldes, vil den opnåede score og den minimale score, der skal opfyldes, blive vist, sammen med anbefalinger til forbedring af scoren.<br><br>
+$lang['PP_passwTitle_d'] = 'Aktivering af denne valgmulighed gør det til at krav at have en adgangskode ved registreringen, og kræver at den valgte adgangskode opfylder et minimalt kompleksitetsniveau.  Hvis tærsklen ikke opfyldes, vil den opnåede score og den minimale score, der skal opfyldes, blive vist, sammen med anbefalinger til forbedring af scoren.<br /><br />
 Der er et testfelt til måling af adgangskodens kompleksitet, og som giver en idé om hvordan man definerer en sådan.<br><br>
+Bemærk: Adgangskodens score beregnes ud fra flere parametre: længde, typen af benyttede tegn (bogstaver, tal, store bogstaver, små bogstaver, særlige tegn). En score på under 100 betragtes som lav, fra 100 til 500 er kompleksiteten gennemsnitlig og over 500 er sikkerheden fremragende.';
 Bemærk: Adgangskodens score beregnes ud fra flere parametre: længde, typen af benyttede tegn (bogstaver, tal, store bogstaver, små bogstaver, særlige tegn). En score på under 100 betragtes som lav, fra 100 til 500 er kompleksiteten gennemsnitlig og over 500 er sikkerheden fremragende.';
 $lang['PP_passwadmTitle_d'] = 'En administrator kan oprette en brugerkonto med eller uden krav om kompleksitetsberegning.<br><br>
 Bemærk: Hvis en brugeren senere ønsker at ændre adgangskode og der er krav om at brugerne har stærke adgangskoder, vil adgangskoden blive udsat for kravet.';
-$lang['PP_PwdResetTitle_d'] = 'Aktivering af denne valgmulighed tilføjer en ny funktion til Piwigos brugerhåndteringspanel (Brugere > Håndter), vedrørende fornyelse af udvalgte brugeres adgangskoder. Desuden tilføjes en ny kolonne, som viser adgangskodestatus for hver af dem, med følgende værdier:<br>
-- Adgangskode skal fornyes: Der er planlagt at bede om fornyelse af adgangskoden.<br>
-- Adgangskode fornyet: Adgangskoden er blevet fornyet, efter der er bedt om det.<br>
-- Oprindelig adgangskode: Den oprindelige adgangskode valgt ved kontooprettelsen, og som aldrig har været krævet fornyet.<br>
-Funktionen gælder ikke webmaster, generiske og gæstekonti.<br>
+$lang['PP_PwdResetTitle_d'] = 'Aktivering af denne valgmulighed tilføjer en ny funktion til håndteringsfanen vedrørende fornyelse af udvalgte brugeres adgangskoder. Desuden tilføjes en ny kolonne, som viser adgangskodestatus for hver af dem, med følgende værdier:<br /><br />
+- Adgangskode skal fornyes: Der er planlagt at bede om fornyelse af adgangskoden.<br />
+- Adgangskode fornyet: Adgangskoden er blevet fornyet, efter der er bedt om det.<br />
+- Oprindelig adgangskode: Den oprindelige adgangskode valgt ved kontooprettelsen, og som aldrig har været krævet fornyet.<br /><br />
+<b style=&quot;color: red;&quot;>Funktionen gælder ikke webmaster, generiske og gæstekonti..</b><br/><br/>
 Disse brugere vil automatisk blive omdirigeret til deres opsætningsside, hver gang de logger på, indtil adgangskoden er blevet ændret, og der vil eksplicit blive givet besked om det på den side.';
 $lang['PP_Unlock'] = 'Lås op';
 $lang['PP_User_Account_Locked_Txt'] = 'Beklager, af sikkerhedsårsager er din adgang til galleriet blevet låst, på grund af for mange mislykkede logonforsøg. Det kan skyldes et forsøg på at bryde ind i din konto. Kontakt galleriets administrator for at bede om at få kontoen låst op.';
-$lang['PP_Webmaster is not unlockable'] = 'Webmaster-brugere kan ikke låses og kan ikke låses op';
+$lang['PP_Webmaster is not unlockable'] = 'Webmasterbrugere kan ikke låses og kan ikke låses op';
 $lang['PP_You cannot unlock your account'] = 'Du kan ikke låse din egen konto op';
 $lang['PP_You need to confirm unlock'] = 'Du skal bekræfte oplåsningen (checkboks)!';
 $lang['PP_Unlock selected users'] = 'Lås valgte brugerkonti op';
 $lang['PP_User Locked'] = 'Bruger låst';
 $lang['PP_User Not Locked'] = 'Bruger ikke låst';
 $lang['PP_UserLocked_Custom_Txt'] = 'Skræddersy den informative besked til den låste bruger';
-$lang['PP_UserLocked_Custom_Txt_d'] = 'Her kan man ændre teksten på den besked, som vises for brugere, hvis konto er blevet låst. For at benytte flere sprog, kan man tags fra plugin\'en Extended Description, hvis denne er aktiv.';
+$lang['PP_UserLocked_Custom_Txt_d'] = 'Her kan man ændre teksten på den besked, som vises for brugere, hvis konto er blevet låst. For at benytte flere sprog, kan man benytte tags fra plugin\'en Extended Description, hvis denne er aktiv.';
 $lang['PP_Admins is not unlockable'] = 'Administrative brugere kan ikke låses og kan ikke låses op';
 $lang['PP_Default user is not unlockable'] = 'Standardbrugeren kan ikke låses og kan ikke låses op';
 $lang['PP_Generic is not unlockable'] = 'Generiske brugere kan ikke låses og kan ikke låses op';
@@ -84,9 +85,9 @@ $lang['PP_Guest is not unlockable'] = 'Gæstebrugere kan ikke låses og kan ikke
 $lang['PP_LockedUsers'] = 'Låsningstilstand';
 $lang['PP_LoginAttempts'] = 'Håndtering af mislykkede logonforsøg';
 $lang['PP_LoginAttempts_d'] = 'Valgmuligheden aktiverer registrering af mislykkede logonforsøg til galleriet på grund af forkert adgangskode, og låser automatisk den pågældende brugers konto. Det har til formål at misvirke mulige hackingforsøg ved hjælp af adgangskodelister.<br/><br/>
-<b style=&quot;color: red;&quot;>Webmaster, generiske og gæstekonti er ikke medtaget i funktionen.</b><br/><br/>
-Ved aktivering af funktionen, får man mulighed for at angive det maksimalt tilladte antal mislykkede forsøg, før låsning, samt man kan definere en skræddersyet besked til brugeren, hvis konto er blevet låst. Beskeden vises hvis det lykkes brugeren at angive de korrekte oplysninger efter en låsning.<br/><br/>
-For at låse konto op, går man til sit galleris brugerhåndteringsgrænseflade.  Dér er der en ny kolonne, som angiver kontostatus markeret med et rødt symbol (for låste konti) og et grønt (for ulåste konti). Vælg en eller flere konti, der skal låses op, og benyt oplåsningsknappen beregnet dertil.';
+<b style=&quot;color: red;&quot;>Webmaster, generiske og gæstekonti er ikke medtaget i funktionen.</b><br /><br />
+Ved aktivering af funktionen, får man mulighed for at angive det maksimalt tilladte antal mislykkede forsøg, før låsning, samt man kan definere en skræddersyet besked til brugeren, hvis konto er blevet låst. Beskeden vises hvis det lykkes brugeren at angive de korrekte oplysninger efter en låsning.<br /><br />
+For at låse konti op, går man til sit galleris brugerhåndteringsgrænseflade.  Dér er der en ny kolonne, som angiver kontostatus markeret med et rødt symbol (for låste konti) og et grønt (for ulåste konti). Vælg en eller flere konti, der skal låses op, og benyt oplåsningsknappen beregnet dertil.';
 $lang['PP_Max number of failed attempts'] = 'Maksimalt antal mislykkede logonforsøg:';
 $lang['PP_Max number of failed attempts_d'] = 'Tallet 0 deaktiverer optælling af logonforsøg.';
 $lang['PP %d user unlocked'] = '%d bruger oplåst';
