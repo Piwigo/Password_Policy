9e33394fb364ea36c55b67070d3e7cf9f2277db7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-12 11:08:20 +0000

    [password_policy] Update sk_SK, thanks to : JoeKundlak
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@27340 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PP_passwtestTitle'] = 'Testujem zložitosť hesla';
+$lang['PP_passwtestTitle_d'] = 'Zadajte testovacie heslo a potom kliknite na &quot;Výpočet skóre&quot; pre získanie výsledku.';
+$lang['PP_UserLocked_Custom_Txt'] = 'Upraviť informačnú správu pre uzamknutého užívateľa';
+$lang['PP_Webmaster cannot be pwdreset'] = 'Pre účet webmastera nie je možné nastaviť obnovenie hesla!';
+$lang['PP_Webmaster is not unlockable'] = 'Účty webmasterov nemôžu byť uzamknuté ani odomknuté';
+$lang['PP_You cannot pwdreset your account'] = 'Pre Váš účet nemôže byť nastavené obnovenie hesla!';
+$lang['PP_You cannot unlock your account'] = 'Nemôžete odomknúť svoj vlastný účet';
+$lang['PP_You need to confirm pwdreset'] = 'Musíte potvrdiť obnovenie hesla (zaškrtnúť políčko)!';
+$lang['PP_You need to confirm unlock'] = 'Musíte potvrdiť odomknutie (zaškrtnúť políčko)!';
+$lang['PP_config_tab'] = 'Nastavenie';
+$lang['PP_save_config'] = 'Nastavenia uložené.';
+$lang['PP_submit'] = 'Poslať nastavenia';
+$lang['PP_PwdReset_Done'] = 'Heslo obnovené';
+$lang['PP_PwdReset_NA'] = 'Pôvodné heslo';
+$lang['PP_PwdReset_Todo'] = 'Heslo, ktoré sa má obnoviť';
+$lang['PP_Pwd_Actions'] = 'Spravovanie užívateľov';
+$lang['PP_ScoreTest'] = 'Výsledok:';
+$lang['PP_Select page number'] = 'Zvoliť číslo stránky pre zobrazenie';
+$lang['PP_Select page size'] = 'Zvoliť veľkosť stránky';
+$lang['PP_SubTitle'] = 'Nastavenie zásuvného modulu';
+$lang['PP_Title'] = 'Pravidlá pre heslá - verzia:';
+$lang['PP_Unlock'] = 'Odomknutie';
+$lang['PP_Unlock selected users'] = 'Odomknúť zvolených užívateľov';
+$lang['PP_User Locked'] = 'Užívateľ uzamknutý';
+$lang['PP_User Not Locked'] = 'Užívateľ nie je uzamknutý';
+$lang['PP_Users_List_Tab'] = 'Spravovanie';
+$lang['PP_Password reset selected users'] = 'Vyžiadať obnovenie hesla pre zvolených užívateľov';
+$lang['PP_PasswordTest'] = 'Výpočet skóre';
+$lang['PP_Password_Enforced_true'] = 'Povoliť. Minimálne skóre:';
+$lang['PP_Disable'] = 'Zakázať (predvolené)';
+$lang['PP_Enable'] = 'Povoliť';
+$lang['PP_Error_Password_Mandatory'] = 'Bezpečnosť: Heslo je povinné!';
+$lang['PP_Generic cannot be pwdreset'] = 'Pre všeobecné účty nie je možné nastaviť obnovenie hesla!';
+$lang['PP_Generic is not unlockable'] = 'Všeobecní užívatelia nemôžu byť uzamknutí a odomknutí';
+$lang['PP_Guest cannot be pwdreset'] = 'Pre účty hostí nie je možné nastaviť obnovenie hesla!';
+$lang['PP_Guest is not unlockable'] = 'Hostia nemôžu byť uzamknutí a odomknutí';
+$lang['PP_LockedUsers'] = 'Stav uzakmnutia';
+?>
