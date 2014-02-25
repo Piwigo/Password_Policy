63e766f15bbeccc68327aa8227dd16b45e165524
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-25 11:20:36 +0000

    [password_policy] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@27488 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -57,4 +57,35 @@ $lang['PP_Generic is not unlockable'] = 'Všeobecní užívatelia nemôžu byť 
 $lang['PP_Guest cannot be pwdreset'] = 'Pre účty hostí nie je možné nastaviť obnovenie hesla!';
 $lang['PP_Guest is not unlockable'] = 'Hostia nemôžu byť uzamknutí a odomknutí';
 $lang['PP_LockedUsers'] = 'Stav uzakmnutia';
+$lang['PP_Pwd_Actions_d'] = 'Uvoľnenie účtov a žiadosti o obnovenie hesla je nastavené tu.<br/><br/>
+Vybrať užívateľov pred nastavením akcie. Políčko potvrdenie je povinné pred použitím akejkoľvek akcie.<br/><br/>
+<b style=&quot;color: red;&quot;>Dôležité :</b> Akcia platí <u><b>LEN</b> pre účty návštevníkov</u> a nie administrátorov, webmastrov, predvolené a hosťa.';
+$lang['PP_Support_txt'] = 'Oficiálna podpora pre tento doplnok je len na týchto Piwigo Forum topic: <br/>
+<a href="http://piwigo.org/forum/viewtopic.php?id=22863" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=22863</a>';
+$lang['PP_UserLocked_Custom_Txt_d'] = 'Tu môžete zmeniť text správy zobrazenej pre používateľa, ktorého účet bol uzamknutý. Ak chcete použiť viac jazykov, môžete použiť tagy na Rozšírený Popis plugin, ak je aktívny.';
+$lang['PP_User_Account_Locked_Txt'] = 'Ospravedlňujeme sa, ale z bezpečnostných dôvodov váš prístup do galérie bol uzamknutý z dôvodu príliš mnohých neúspešných pokusov o prihlásenie. To môže byť spôsobené pokusmi o vniknutie do vášho účtu. Prosím požiadajte o odblokovanie správcu webu.';
+$lang['PP_passwadmTitle_d'] = 'Správca môže vytvoriť užívateľský účet s alebo bez použitia pravidla o výpočtovej zložitosti.<br/><br/>
+Poznámka: Ak pre vytvorený užívateľský účet chcete zmeniť heslo a posilniť ho pre aktívneho užívateľa, bude podliehať sade pravidiel.';
+$lang['PP_Error_Password_Need_Enforcement_%s'] = 'Bezpečnosť: Riadiaci systém vypočítava skóre pre zvolené heslo podľa zložitosti. Zložitosť hesla je príliš nízka (skóre =%s). Prosím, vyberte nové heslo bezpečnejšie podľa nasledujúcich pravidiel: <br/>
+- Používajte písmená a čísla <br/>
+- Používajte malé a veľké písmená <br/>
+- Zvýšte svoju dĺžku (počet znakov) <br/>
+Minimálny požadovaný počet bodov hesla správcov je:';
+$lang['PP_PwdReset'] = 'Obnova hesla';
+$lang['PP_Password_Reset_Msg'] = 'Prosím, zmeňte svoje heslo!';
+$lang['PP_Password_Enforced'] = 'Posilnenie úrovne zabezpečenia hesla';
+$lang['PP_No_Userlist'] = 'Žiadni užívatelia nie sú k dispozícii na zobrazenie';
+$lang['PP_Max number of failed attempts_d'] = 'Číslo 0 zakáže počítanie pokusov.';
+$lang['PP_Max number of failed attempts'] = 'Maximálny počet neúspešných pokusov:';
+$lang['PP_LoginAttempts'] = 'Riadenie neúspešných pokusov o prihlásenie';
+$lang['PP_Err_Userlist_Settings'] = 'Táto karta je k dispozícii len v prípade, že je povolené obnovenie hesla alebo odblokovanie účtov';
+$lang['PP_Default user is not unlockable'] = 'Predvolený používateľ nemôže byť uzamknutý ani odomknutý';
+$lang['PP_Default user cannot be pwdreset'] = 'Obnovu vyžadovaného hesla nie je možné nastaviť pre predvolený používateľský účet!';
+$lang['PP_Admins cannot be pwdreset'] = 'Obnova vyžadovaného hesla nemôže byť poslaná pre administrátorský účet!';
+$lang['PP_Admins is not unlockable'] = 'Administrátorskí používatelia nemôžu byť uzamknutí ani odomknutí';
+$lang['PP %d users pwdreseted'] = 'Obnova vyžadovaného hesla pre používateľov %d ';
+$lang['PP %d user pwdreseted'] = 'Obnova vyžadovaného hesla pre používateľa %d';
+$lang['PP_AdminPassword_Enforced'] = 'Pridať do administrátorov';
+$lang['PP %d users unlocked'] = '%d používateľov je odblokovaných';
+$lang['PP %d user unlocked'] = '%d používateľ je odblokovaný';
 ?>
