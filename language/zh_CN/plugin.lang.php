1c7af91d90ee23e80725e6d01c6da5af9d92789a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-27 17:15:02 +0000

    [password_policy] Update zh_CN, thanks to : junwang
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@27504 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PP_LockedUsers'] = '锁定状态';
+$lang['PP_LoginAttempts'] = '无法登录管理';
+$lang['PP_Guest is not unlockable'] = '来宾帐户不能被锁定和解锁';
+$lang['PP_Admins is not unlockable'] = '管理员账户不能被锁定和解锁';
+$lang['PP_Guest cannot be pwdreset'] = '不能设置来宾帐户的密码更新!';
+$lang['PP_Generic is not unlockable'] = '一般用户不能被锁定，也不能解锁';
+$lang['PP_Generic cannot be pwdreset'] = '通用账户不能更新密码！';
+$lang['PP_Error_Password_Need_Enforcement_%s'] = '安全：控制系统计算所选择的密码的复杂性。你的密码的复杂度太低（得分= %s）。请用以下规则选择一个更安全的新密码：<br/>
+- 使用字母和数字<br/>
+- 使用小写和大写的<br/>
+- 增加密码长度（字符数）<br />
+管理员要求密码的最低要求分数是：';
+$lang['PP_Error_Password_Mandatory'] = '安全：密码是强制性的！';
+$lang['PP_Err_Userlist_Settings'] = '这个标签只有在密码已更新或帐户已解锁后才可用';
+$lang['PP_Default user is not unlockable'] = '缺省用户不能被锁和解锁';
+$lang['PP_Disable'] = '禁用（缺省）';
+$lang['PP_Enable'] = '使能够';
+$lang['PP %d users unlocked'] = '%d用户已解锁';
+$lang['PP_Default user cannot be pwdreset'] = '缺省用户的账户不能更新密码';
+$lang['PP_Admins cannot be pwdreset'] = '管理员账户密码不能更新';
+$lang['PP_AdminPassword_Enforced'] = '适用于管理员';
+$lang['PP %d users pwdreseted'] = '%d用户需要更新密码';
+$lang['PP %d user unlocked'] = '%d用户已解锁';
+$lang['PP %d user pwdreseted'] = '%d用户需要更新密码';
+?>
