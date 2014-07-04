7c3350af226e3f3fbaaa1a589467484809af7d55
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-04 16:50:48 +0000

    [password_policy] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@28940 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -25,23 +25,22 @@ $lang['PP_LoginAttempts'] = '无法登录管理';
 $lang['PP_Guest is not unlockable'] = '来宾帐户不能被锁定和解锁';
 $lang['PP_Admins is not unlockable'] = '管理员账户不能被锁定和解锁';
 $lang['PP_Guest cannot be pwdreset'] = '不能设置来宾帐户的密码更新!';
-$lang['PP_Generic is not unlockable'] = '一般用户不能被锁定，也不能解锁';
+$lang['PP_Generic is not unlockable'] = '通用账户不能被锁定和解锁';
 $lang['PP_Generic cannot be pwdreset'] = '通用账户不能更新密码！';
 $lang['PP_Error_Password_Need_Enforcement_%s'] = '安全：控制系统计算所选择的密码的复杂性。你的密码的复杂度太低（得分= %s）。请用以下规则选择一个更安全的新密码：<br/>
 - 使用字母和数字<br/>
 - 使用小写和大写的<br/>
 - 增加密码长度（字符数）<br />
 管理员要求密码的最低要求分数是：';
-$lang['PP_Error_Password_Mandatory'] = '安全：密码是强制性的！';
-$lang['PP_Err_Userlist_Settings'] = '这个标签只有在密码已更新或帐户已解锁后才可用';
-$lang['PP_Default user is not unlockable'] = '缺省用户不能被锁和解锁';
-$lang['PP_Disable'] = '禁用（缺省）';
-$lang['PP_Enable'] = '使能够';
-$lang['PP %d users unlocked'] = '%d用户已解锁';
-$lang['PP_Default user cannot be pwdreset'] = '缺省用户的账户不能更新密码';
+$lang['PP_Error_Password_Mandatory'] = '安全：密码是必须的！';
+$lang['PP_Err_Userlist_Settings'] = '此标签仅在密码更新或帐户解锁被启用时才可用';
+$lang['PP_Default user is not unlockable'] = '默认用户不能被锁定和解锁';
+$lang['PP_Disable'] = '禁用（默认）';
+$lang['PP_Enable'] = '启用';
+$lang['PP %d users unlocked'] = '%d个用户已解锁';
+$lang['PP_Default user cannot be pwdreset'] = '默认用户的账户不能更新密码';
 $lang['PP_Admins cannot be pwdreset'] = '管理员账户密码不能更新';
-$lang['PP_AdminPassword_Enforced'] = '适用于管理员';
-$lang['PP %d users pwdreseted'] = '%d用户需要更新密码';
-$lang['PP %d user unlocked'] = '%d用户已解锁';
-$lang['PP %d user pwdreseted'] = '%d用户需要更新密码';
-?>
+$lang['PP_AdminPassword_Enforced'] = '应用于管理员';
+$lang['PP %d users pwdreseted'] = '%d个用户需要更新密码';
+$lang['PP %d user unlocked'] = '%d个用户已解锁';
+$lang['PP %d user pwdreseted'] = '%d个用户需要更新密码';
