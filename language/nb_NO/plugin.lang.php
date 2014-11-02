82b40bcf67e43c8ed1d63052c3070031387537bd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-02 08:07:38 +0000

    [password_policy] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Password_Policy@30340 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['PP %d user pwdreseted'] = 'Passord fornyelse er nødvendig for %d bruker';
+$lang['PP %d user unlocked'] = '%d bruker koblet fra';
+$lang['PP %d users pwdreseted'] = 'Passord fornyelse er nødvendig for %d brukere';
+$lang['PP %d users unlocked'] = '%d brukere koblet fra';
+$lang['PP_AdminPassword_Enforced'] = 'Søknad til administratorer';
+$lang['PP_Admins cannot be pwdreset'] = 'Passord fornyelse kan ikke lagres for admins konto!';
+$lang['PP_Admins is not unlockable'] = 'Admin-brukere kan ikke låses og kan ikke låses opp';
+$lang['PP_Default user cannot be pwdreset'] = 'Passord fornyelse kan ikke lagres for standard brukerkonto!';
+$lang['PP_Default user is not unlockable'] = 'Standard bruker kan ikke låses og kan ikke låses opp';
+$lang['PP_Disable'] = 'Deaktiver (standard)';
+$lang['PP_Enable'] = 'Aktivert';
+$lang['PP_Err_Userlist_Settings'] = 'Denne kategorien er bare tilgjengelig hvis passord fornyelse eller låse opp kontoer er aktivert';
+$lang['PP_Error_Password_Mandatory'] = 'Sikkerhet: Passord er obligatorisk!';
+$lang['PP_Error_Password_Need_Enforcement_%s'] = 'Sikkerhet: Et kontrollsystem beregner en poengsum på valgt passord kompleksitet. Kompleksiteten i passordet ditt er for lavt (poengsum =%s). Vennligst, velg et sikrere passord  ved å følge disse reglene:<br/>
+- Bruk bokstaver og tall<br/>
+- Bruk små og store bokstaver<br/>
+- Øk lengden (antall tegn)<br/>
+Minste passord scorer påkrevd av administrator er:';
+$lang['PP_Generic cannot be pwdreset'] = 'Passord fornyelse kan ikke lagres for generiske kontoer!';
+$lang['PP_Generic is not unlockable'] = 'Generiske brukere kan ikke låses og kan ikke låses opp';
+$lang['PP_Guest cannot be pwdreset'] = 'Passord fornyelse kan ikke lagres for gjeste konto!';
+$lang['PP_Guest is not unlockable'] = 'Gjestebrukere kan ikke låses og kan ikke låses opp';
+$lang['PP_LockedUsers'] = 'Låse bekreftelse';
+$lang['PP_LoginAttempts'] = 'Forvaltningen av mislykkede innloggingsforsøk';
